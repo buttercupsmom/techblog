49bec8a026180fb40eae8debd9e3b78dc72a884a
Rachel <yarkony.rachel@gmail.com>
2022-03-31 14:35:35 -0500

    first commit

---
+++ seeds/seed.js
@@ -0,0 +1,33 @@
+const sequelize = require('../config/connection');
+const { User, Post, Comment } = require('../models');
+
+const userData = require('./userData.json');
+const postData = require('./postData.json');
+const commentData = require('./commentData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+  // Making users
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+  // making posts
+  for (const post of postData) {
+    await Post.create({
+      ...post,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+  // creating comments
+  for (const comment of commentData) {
+    await Comment.create({
+      ...comment,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+      post_id: Math.ceil(Math.random() * commentData.length),
+    });
+  }
+  process.exit(0);
+};
+
+seedDatabase();
