51829ee70ee5362afd398d13b8020e43d5dd6d94
Rachel <yarkony.rachel@gmail.com>
2022-04-25 13:27:47 -0500

    delete button clickable, needs to be routed

--- public/js/posts.js
+++ public/js/posts.js
@@ -1,6 +1,5 @@
 const newFormHandler = async function (event) {
   event.preventDefault();
-
   const title = document.querySelector('#comment-desc').value;
   const post_id = document.querySelector('input[name="post_id"]').value;
   console.log(title, post_id);
@@ -19,4 +18,12 @@ const newFormHandler = async function (event) {
   }
 };
 
+const deleteComment = async function (event) {
+  event.preventDefault();
+  const deleteButton = document.querySelector('#deleteComment').dataset.action;
+  console.log(deleteButton);
+};
+
 document.querySelector('.new-comment-form').addEventListener('submit', newFormHandler);
+
+document.querySelector('.delete-comment-form'), addEventListener('submit', deleteComment);
